223fda9fbc9f399ff47f9096be4d50b7f87f54b7
i-made-that <rogerthomasson@gmail.com>
2015-04-24 16:58:12 -0700

    (add)

---
+++ client/sender-app/core/directives.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.directives')
+    .directive('ngEnterKey', ngEnterKey);
+
+    function ngEnterKey() {
+      return function(scope, element, attrs) {
+
+        element.bind('keydown keypress', function(event) {
+          var keyCode = event.which || event.keyCode;
+
+          // If enter key is pressed
+          if (keyCode === 13) {
+            scope.$apply(function() {
+                // Evaluate the expression
+                scope.$eval(attrs.ngEnterKey);
+            });
+
+            event.preventDefault();
+          }
+        });
+      };
+    }
+})();
